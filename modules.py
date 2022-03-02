50ab50e25b3c34bc22c3c50bac25fc1e055b8b51
Mooncakk <85755431+Mooncakk@users.noreply.github.com>
2022-03-02 19:47:41 +0100

    Delete modules.py

--- modules.py
+++
@@ -1,6 +0,0 @@
-from utills import GestionFichierJson as GFJ
-from gestion_joueurs_et_tournois import *
-from modification_liste_joueurs import *
-from chess_program import RetourMenuJoueur
-from algorithm import *
-
